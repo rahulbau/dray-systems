ea4450152de4021b32ada50cc4a4364e02dd9e26
Rahul * <rahul.4@globallogic.com>
2022-01-27 21:42:19 +0530

    token removed for get organization api

--- modules/user/index.js
+++ modules/user/index.js
@@ -7,7 +7,7 @@ const userController = require('./controllers/userController');
 router.put('',userValidator.editUser, authMiddleware.auth, userController.editUser);
 router.get('', authMiddleware.auth, userController.getUser);
 router.get('/users', authMiddleware.auth, userController.getUsers);
-router.get('/organizations',userValidator.getOrganizations, authMiddleware.auth, userController.getOrganizations);
+router.get('/organizations',userValidator.getOrganizations, userController.getOrganizations);
 router.post('/organizations',userValidator.addOrganizations,  userController.addOrganizations);
 
 
